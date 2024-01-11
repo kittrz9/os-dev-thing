cc93970ee984f06619ded4b4e66f5afb2db1c9dd
kittrz <kittrz@kittrz.gay>
2024-01-10 23:17:06 -0600

    move screen stuff to its own files

---
+++ src/stage2/screen.c
@@ -0,0 +1,22 @@
+#include "screen.h"
+
+// maximum saturation and value
+uint32_t hueToRgb(uint8_t hue) {
+	// https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+	uint8_t x = 256-(hue*6 % 255);
+	if(hue < 43) {
+		return 0xff0000 | (256-x)<<8;
+	} else if(hue < 85) {
+		return 0x00ff00 | x<<16;
+	} else if(hue < 128) {
+		return 0x00ff00 | (256-x);
+	} else if(hue < 170) {
+		return 0x0000ff | x<<8;
+	} else if(hue < 213) {
+		return 0x0000ff | (uint8_t)(256-x)<<16; // to avoid overflow shenanigans
+	} else {
+		return 0xff0000 | x;
+	}
+}
+
+
--- src/stage2/stage2.c
+++ src/stage2/stage2.c
@@ -1,64 +1,6 @@
 #include "serial.h"
 #include "io.h"
-
-// maximum saturation and value
-uint32_t hueToRgb(uint8_t hue) {
-	// https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
-	uint8_t x = 256-(hue*6 % 255);
-	if(hue < 43) {
-		return 0xff0000 | (256-x)<<8;
-	} else if(hue < 85) {
-		return 0x00ff00 | x<<16;
-	} else if(hue < 128) {
-		return 0x00ff00 | (256-x);
-	} else if(hue < 170) {
-		return 0x0000ff | x<<8;
-	} else if(hue < 213) {
-		return 0x0000ff | (uint8_t)(256-x)<<16; // to avoid overflow shenanigans
-	} else {
-		return 0xff0000 | x;
-	}
-}
-
-// literally just copied and pasted from osdev wiki
-// https://wiki.osdev.org/Getting_VBE_Mode_Info#VESA_Functions
-struct vbe_mode_info_structure {
-	uint16_t attributes;		// deprecated, only bit 7 should be of interest to you, and it indicates the mode supports a linear frame buffer.
-	uint8_t window_a;			// deprecated
-	uint8_t window_b;			// deprecated
-	uint16_t granularity;		// deprecated; used while calculating bank numbers
-	uint16_t window_size;
-	uint16_t segment_a;
-	uint16_t segment_b;
-	uint32_t win_func_ptr;		// deprecated; used to switch banks from protected mode without returning to real mode
-	uint16_t pitch;			// number of bytes per horizontal line
-	uint16_t width;			// width in pixels
-	uint16_t height;			// height in pixels
-	uint8_t w_char;			// unused...
-	uint8_t y_char;			// ...
-	uint8_t planes;
-	uint8_t bpp;			// bits per pixel in this mode
-	uint8_t banks;			// deprecated; total number of banks in this mode
-	uint8_t memory_model;
-	uint8_t bank_size;		// deprecated; size of a bank, almost always 64 KB but may be 16 KB...
-	uint8_t image_pages;
-	uint8_t reserved0;
- 
-	uint8_t red_mask;
-	uint8_t red_position;
-	uint8_t green_mask;
-	uint8_t green_position;
-	uint8_t blue_mask;
-	uint8_t blue_position;
-	uint8_t reserved_mask;
-	uint8_t reserved_position;
-	uint8_t direct_color_attributes;
- 
-	uint32_t framebuffer;		// physical address of the linear frame buffer; write here to draw to the screen
-	uint32_t off_screen_mem_off;
-	uint16_t off_screen_mem_size;	// size of memory in the framebuffer but not being displayed on the screen
-	uint8_t reserved1[206];
-} __attribute__ ((packed));
+#include "screen.h"
 
 void loadIDT(void);
 void test(void) {
